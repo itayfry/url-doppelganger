e95b33f958343dbf677726b21b104473dbe0bdfd
Royce <roycebradley@cmail.carleton.ca>
2020-10-06 18:44:42 -0400

    Added persistent storage+ requested changes

--- popup.js
+++ popup.js
@@ -16,22 +16,36 @@ let buttonsConfig = [
     }
 ]
 
+window.onload = function() {
+  var labels = document.getElementsByClassName('labels');
+  var protocols = document.getElementsByClassName('protocols');
+  var params = document.getElementsByClassName('params');
+  for(var i = 0; i < labels.length; i++){
+    if(sessionStorage.getItem("labels" + [i]) != null){
+      buttonsConfig[i].label = sessionStorage.getItem("labels" + [i]);
+    }
+    if(sessionStorage.getItem("protocols" + [i]) != null){
+      buttonsConfig[i].protocol = sessionStorage.getItem("protocols" + [i]);
+    }
+    if(sessionStorage.getItem("params" + [i]) != null){
+      buttonsConfig[i].params = sessionStorage.getItem("params" + [i]);
+    }
+  }
+}
+
 
 document.getElementById('edit').addEventListener("change", function(){
 
   if(document.getElementById('edit').checked){
-    var elems = document.getElementsByClassName('editSettings');
-    for(var i = 0; i < elems.length; i++){
-      elems[i].style.display = "inline";
-    }
+    var elems = document.getElementsByClassName('settingsContainer');
+    elems[0].style.display = "block";
   }
   else{
-    var elems = document.getElementsByClassName('editSettings');
-    for(var i = 0; i < elems.length; i++){
-      elems[i].style.display = "none";
-    }
+    var elems = document.getElementsByClassName('settingsContainer');
+    elems[0].style.display = "none";
   }
 })
+
 
 document.getElementById('submit').addEventListener('click', function(){
   var labels = document.getElementsByClassName('labels');
@@ -40,25 +54,40 @@ document.getElementById('submit').addEventListener('click', function(){
 
   for(var i = 0; i < labels.length; i++){
     if(labels[i].value != ""){
-      buttonsConfig[i].label = labels[i].value;
-    }
+      sessionStorage.setItem("labels" + [i], labels[i].value );
+      buttonsConfig[i].label = labels[i].value;//
+
+      }
     if(protocols[i].value != ""){
+      sessionStorage.setItem("protocols" + [i], protocols[i].value );
       buttonsConfig[i].protocol = protocols[i].value;
     }
     if(params[i].value != ""){
-      buttonsConfig[i].params = params[i].value;
+      sessionStorage.setItem("params" + [i], JSON.parse(params[i].value));
+      buttonsConfig[i].params = JSON.parse(params[i].value);
     }
   }
+
+
+  console.log(buttonsConfig[0].label);
+  console.log(buttonsConfig[1].label);
+  console.log(buttonsConfig[2].label);
+  console.log(buttonsConfig[0].protocol);
+  console.log(buttonsConfig[1].protocol);
+  console.log(buttonsConfig[2].protocol);
   console.log(buttonsConfig[0].params);
   console.log(buttonsConfig[1].params);
   console.log(buttonsConfig[2].params);
 
-  var elems = document.getElementsByClassName('editSettings');
-  for(var i = 0; i < elems.length; i++){
-    elems[i].style.display = "none";
-  }
+
+
+  // var elems = document.getElementsByClassName('editSettings');
+  // for(var i = 0; i < elems.length; i++){
+  //   elems[i].style.display = "none";
+  // }
+
   document.getElementById('edit').checked = false;
-  alert("Save Succuessfully!")
+  alert("Saved Succuessfully!")
 })
 
 
